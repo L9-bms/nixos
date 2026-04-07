997cdfe7d21d9803e4e5910afdf5a77021be4a7d
Callum <mail@callumwong.com>
2026-04-08 01:55:01 +1000

    simplify mc server config

--- modules/services/mc-server.nix
+++ modules/services/mc-server.nix
@@ -1,68 +1,46 @@
 {
-  flake.modules.nixos.mc-server =
-    { config, ... }:
-    {
-      virtualisation.quadlet =
-        let
-          inherit (config.virtualisation.quadlet) networks pods volumes;
-        in
-        {
-          containers = {
-            minecraft-server = {
-              serviceConfig = {
-                RestartSec = "10";
-                Restart = "always";
-                TimeoutStartSec = "120";
-              };
-              containerConfig = {
-                image = "itzg/minecraft-server:java17";
-                podmanArgs = [
-                  "--attach"
-                  "stdin"
-                  "--tty"
-                ];
-                pod = pods.minecraft.ref;
-                environments = {
-                  EULA = "TRUE";
-                  VERSION = "1.19.2";
-                  MEMORY = "6144M";
-                  USE_MEOWICE_FLAGS = "true";
-                  TYPE = "FORGE";
-                  # PACKWIZ_URL = "https://asphodel.cc/packwiz/Ports/Curse/Raspberry-Server/pack.toml";
-                  MODRINTH_PROJECTS = ''
-                    # proxy-compatible-forge
-                    proxy-protocol-support:1.1.0-forge
-                    spark
-                    chunky
-                    collective
-                    # beautified-chat-server
-                    no-telemetry
-                    keepheadnames
-                    # grieflogger
-                  '';
-                };
-                exposePorts = [ "25565" ];
-                volumes = [ "${volumes.minecraft.ref}:/data" ];
-                stopTimeout = 60;
-                # healthcheck will always fail
-                healthInterval = "disable";
-              };
-            };
-          };
-          volumes.minecraft.volumeConfig = {
-            type = "bind";
-            device = "/data/cottage-witch";
-            # for some reason i couldn't change what the bind mount pointed to, maybe open an issue upstream
-          };
-          networks = {
-            internal.networkConfig.subnets = [ "10.0.67.1/24" ];
-          };
-          pods = {
-            minecraft.podConfig = {
-              networks = [ networks.internal.ref ];
-              publishPorts = [ "25565:25565" ];
-            };
-          };
+  flake.modules.nixos.mc-server = {
+    virtualisation.quadlet.containers.minecraft-server = {
+      serviceConfig = {
+        RestartSec = "10";
+        Restart = "always";
+        TimeoutStartSec = "120";
+      };
+      containerConfig = {
+        image = "itzg/minecraft-server:java17";
+        podmanArgs = [
+          "--attach"
+          "stdin"
+          "--tty"
+          "--publish"
+          "25565:25565"
+        ];
+        environments = {
+          EULA = "TRUE";
+          VERSION = "1.19.2";
+          MEMORY = "6144M";
+          USE_MEOWICE_FLAGS = "true";
+          TYPE = "FORGE";
+          # PACKWIZ_URL = "https://asphodel.cc/packwiz/Ports/Curse/Raspberry-Server/pack.toml";
+          MODRINTH_PROJECTS = ''
+            # proxy-compatible-forge
+            proxy-protocol-support:1.1.0-forge
+            spark
+            chunky
+            collective
+            # beautified-chat-server
+            no-telemetry
+            keepheadnames
+            # grieflogger
+          '';
         };
+        exposePorts = [ "25565" ];
+        # volumes = [ "/data/raspberry:/data" ];
+        volumes = [ "/data/cottage-witch:/data" ];
+        stopTimeout = 60;
+        # healthcheck will always fail
+        healthInterval = "disable";
+      };
     };
+  };
 }
