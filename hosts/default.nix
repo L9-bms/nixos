fc7b41873c4dec1757ba37482d0fe072f5721454
Callum <mail@callumwong.com>
2026-04-11 23:16:04 +1000

    abstraction for host definitions

---
+++ hosts/default.nix
@@ -0,0 +1,43 @@
+{
+  config,
+  inputs,
+  lib,
+  ...
+}:
+let
+  mkHosts =
+    prefix: modules: builder:
+    lib.mapAttrs' (
+      name: module:
+      let
+        hostname = lib.removePrefix prefix name;
+      in
+      {
+        name = hostname;
+        value = builder hostname module;
+      }
+    ) (lib.filterAttrs (name: _: lib.hasPrefix prefix name) modules);
+in
+{
+  flake.nixosConfigurations = mkHosts "hosts/nixos/" config.flake.modules.nixos (
+    hostname: module:
+    inputs.nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        { networking.hostName = hostname; }
+        module
+        inputs.disko.nixosModules.default
+        inputs.quadlet-nix.nixosModules.quadlet
+      ];
+    }
+  );
+
+  flake.deploy.nodes = lib.mapAttrs (hostname: hostConfiguration: {
+    inherit hostname;
+    profiles.system = {
+      user = "root";
+      sshUser = "callum";
+      path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos hostConfiguration;
+    };
+  }) config.flake.nixosConfigurations;
+}
--- hosts/nixos/staging/default.nix
+++ hosts/nixos/staging/default.nix
@@ -1,33 +1,6 @@
+{ config, ... }:
 {
-  config,
-  inputs,
-  ...
-}:
-let
-  hostname = "staging";
-  system = "x86_64-linux";
-in
-{
-  flake.deploy.nodes.${hostname} = {
-    inherit hostname;
-    profiles.system = {
-      user = "root";
-      sshUser = "callum";
-      path = inputs.deploy-rs.lib.${system}.activate.nixos config.flake.nixosConfigurations.${hostname};
-    };
-  };
-
-  flake.nixosConfigurations.${hostname} = inputs.nixpkgs.lib.nixosSystem {
-    inherit system;
-    modules = [
-      { networking.hostName = hostname; }
-      config.flake.modules.nixos.${hostname}
-      inputs.disko.nixosModules.default
-      inputs.quadlet-nix.nixosModules.quadlet
-    ];
-  };
-
-  flake.modules.nixos.${hostname} = {
+  flake.modules.nixos."hosts/nixos/staging" = {
     imports =
       (with config.flake.nixosModules; [
         staging-disko
