b99da94cfd5427a6009c59122b902029a72a850d
callum <mail@callumwong.com>
2026-05-11 01:18:17 +1000

    update callum@wky ssh key

--- modules/users/callum.nix
+++ modules/users/callum.nix
@@ -11,7 +11,7 @@
             initialPassword = "changeme";
             openssh.authorizedKeys.keys = [
               "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMP4bm4SjbUcveDfeNVU7QkWz/pFWuVrPsZIa5e6ZE64 callum@acid"
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINw8zK93i7WJYfbmpcXE5ZYTWRvkm3ohIdsvWmWOkCFQ callum@wky"
+              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICDCd6wji8oxyVnqDq3I5qMTOfNJ8Z/Rm/eGfismjUL4 callum@wky"
             ];
           };
           programs.fish.enable = true;
